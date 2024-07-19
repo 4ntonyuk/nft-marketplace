3630499f13e15cabe643989a72d88a05be1643e1
4ntonyuk <uvinnobusu@gmail.com>
2024-07-20 00:33:46 +0300

    репрессии

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-const Home = (): JSX.Element => {
-  return (
-    <div className="font-roboto">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium libero corrupti hic. Adipisci, nostrum possimus quos recusandae consequuntur distinctio dolores vitae magnam hic, at quasi sapiente omnis nobis voluptatum animi.</div>
-  )
-}
-
-export default Home;
